52f89e2b348910a1f097d562f1b2ddf3e3913558
jordz-a <jordannekerrr@gmail.com>
2025-07-09 23:15:48 -0500

    Base

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function checkPassword() {
   const input = document.getElementById('password-input').value;
   const error = document.getElementById('error-message');
 
-  if (input === 'ilovero') {
+  if (input === 'roheartsjakie') {
     document.getElementById('login-screen').style.display = 'none';
     document.getElementById('main-content').style.display = 'block';
   } else {
